418fcef1aec2c7a210afc8fd9ccd86952413caa3
Johannes Sundqvist <johannes@israproductions.com>
2015-03-16 13:07:53 +0100

    Fixed issue where eclipse cannot clean project because webdriver.exe is locked. Driver path now redirected to workspace root.

--- src/main/java/com/scania/saf/WebDrivers.java
+++ src/main/java/com/scania/saf/WebDrivers.java
@@ -20,6 +20,8 @@ import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import com.google.common.io.Files;
+
 public class WebDrivers {
 
 	private static final String FILE_PREFIX = "file:/";
@@ -42,8 +44,7 @@ public class WebDrivers {
 	public static WebDriver getInternetExplorer() {
 		String path = initDriver("IEDriverServer.exe");
 		System.setProperty("webdriver.ie.driver", path);
-		DesiredCapabilities capabilities = DesiredCapabilities
-				.internetExplorer();
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
 		capabilities.setJavascriptEnabled(true);
 		capabilities.setCapability("ACCEPT_SSL_CERTS", true);
 		return new InternetExplorerDriver(capabilities);
@@ -51,23 +52,72 @@ public class WebDrivers {
 
 	private static String initDriver(String driverName) {
 		String sourcePath = findSourcePath();
-		System.out.println("Loading WebDrivers from: "+sourcePath);
-		String targetDirectory = findTargetDirectory();
+		System.out.println("Loading library classes from: "+sourcePath);
+		String targetDirectory = parentDirectory(findClassesDirectory(), 3);
 		File sourceFile = new File(sourcePath);
+		String driverPath;
 		if(sourceFile.isDirectory())
-			return fileName(sourcePath, driverName);
-		return conditionalCopyFromJar(driverName, sourceFile, targetDirectory);
+			driverPath = conditionalCopyFromTargetDirectory(driverName, sourcePath, targetDirectory);
+		driverPath = conditionalCopyFromJar(driverName, sourceFile, targetDirectory);
+		driverPath = new File(driverPath).getAbsolutePath();
+		System.out.println("Loading "+driverName+" from "+driverPath);
+		return driverPath;
+	}
+
+	private static String parentDirectory(String directory, int n) {
+		directory = directory.replace("\\", "/");
+		if(directory.endsWith("/"))
+			directory = directory.substring(0, directory.length()-1);
+		if(n == 0)
+			return directory+"/";
+		return parentDirectory(directory.substring(0, directory.lastIndexOf("/")), n-1);
+	}
+
+	private static String conditionalCopyFromTargetDirectory(String driverName, String sourcePath, String targetDirectory) {
+		File driverFile = new File(filePath(targetDirectory, driverName));
+		if(!driverFile.exists())
+			copyDriverFromTargetDirectory(driverName, sourcePath, targetDirectory);
+		return driverFile.getAbsolutePath();
+	}
+
+	private static void copyDriverFromTargetDirectory(String driverName, String sourceDirectory, String targetDirectory) {
+		File sourceFile = new File(filePath(sourceDirectory, driverName));
+		File targetFile = new File(filePath(targetDirectory, driverName));
+		try {
+			Files.copy(sourceFile, targetFile);
+			System.out.println("Copied "+driverName+" from "+sourceDirectory+" to "+targetDirectory);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String findSourcePath() {
+		return findClassPath(WebDrivers.class);
+	}
+
+	private static String findClassesDirectory() {
+		try {
+			return findClassPath(Class.forName(getCallerClassName()));
+		} catch(ClassNotFoundException e) {
+			return null;
+		}
+	}
+
+	private static String findClassPath(Class<?> type) {
+		String path = type.getProtectionDomain().getCodeSource().getLocation().toString();
+		if(path.startsWith(FILE_PREFIX))
+			path = path.substring(FILE_PREFIX.length());
+		return path;
 	}
 
 	private static String conditionalCopyFromJar(String driverName, File sourceFile, String targetDirectory) {
-		System.out.println("Storing WebDrivers in: "+targetDirectory);
-		File driverFile = new File(fileName(targetDirectory, driverName));
+		File driverFile = new File(filePath(targetDirectory, driverName));
 		if(!driverFile.exists())
 			copyDriverFromJar(driverName, sourceFile, targetDirectory);
 		return driverFile.getAbsolutePath();
 	}
 
-	private static void copyDriverFromJar(String driverName, File source, String targetPath) {
+	private static void copyDriverFromJar(String driverName, File source, String targetDirectory) {
 		JarFile jar = null;
 		try {
 			jar = new JarFile(source);
@@ -76,17 +126,17 @@ public class WebDrivers {
 	            JarEntry entry = entries.nextElement();
 	            if (!entry.isDirectory() && entry.getName().equals(driverName)) {
 	                BufferedInputStream inputStream = new BufferedInputStream(jar.getInputStream(entry));
-	                File targetFile = new File(targetPath+driverName);
+	                File targetFile = new File(targetDirectory+driverName);
 	                FileOutputStream targetStream = new FileOutputStream(targetFile);
 	                BufferedOutputStream outputStream = new BufferedOutputStream(targetStream);
 	                copyStream(inputStream, outputStream);
 	                inputStream.close();
 	                outputStream.close();
-	                return;
+	                break;
 	            }
 	        }
+    		System.out.println("Copied "+driverName+" from "+source.getAbsolutePath()+" to "+targetDirectory);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
@@ -98,39 +148,19 @@ public class WebDrivers {
 		}
 	}
 
-	private static void copyStream(InputStream input, OutputStream output)
-			throws IOException {
+	private static void copyStream(InputStream input, OutputStream output) throws IOException {
 		byte[] buffer = new byte[1024]; // Adjust if you want
 		int bytesRead;
-		while ((bytesRead = input.read(buffer)) != -1)
+		while((bytesRead = input.read(buffer)) != -1)
 			output.write(buffer, 0, bytesRead);
 	}
 
-	private static String fileName(String targetDirectory, String driverName) {
-		return targetDirectory+driverName;
-	}
-
-	private static String findTargetDirectory() {
-		try {
-			return findClassPath(Class.forName(getCallerClassName()));
-		} catch(ClassNotFoundException e) {
-			return null;
-		}
+	private static String filePath(String directory, String name) {
+		return directory+name;
 	}
 
 	private static String getCallerClassName() {
 		return Stack.getStackTraceElement(4).getClassName();
 	}
 	
-	private static String findSourcePath() {
-		return findClassPath(WebDrivers.class);
-	}
-
-	private static String findClassPath(Class<?> type) {
-		String path = type.getProtectionDomain().getCodeSource().getLocation().toString();
-		if(path.startsWith(FILE_PREFIX))
-			path = path.substring(FILE_PREFIX.length());
-		return path;
-	}
-
 }
